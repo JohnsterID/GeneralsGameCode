4df0e72f32144b256010eb732ac5bba81622e72a
JohnsterID <69278611+JohnsterID@users.noreply.github.com>
2026-01-06 08:33:25 +1100

    MinGW-w64 compatibility

--- cmake/dx8.cmake
+++ cmake/dx8.cmake
@@ -1,7 +1,6 @@
 FetchContent_Declare(
     dx8
     GIT_REPOSITORY https://github.com/JohnsterID/min-dx8-sdk.git
-    GIT_TAG        3bb49bc53a47729255d8ee26d94303cf8287f397
+    GIT_TAG        2233fb14d44ae2e8573c4eff463213b7f50c36a0
 )
-
 FetchContent_MakeAvailable(dx8)
